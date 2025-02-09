6b95df586bf68f7023a47a58ed605f1f3097cabc
ykeshav <ykeshav@anutanetworks.com>
2025-02-09 20:06:49 +0530

    length updation

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
                                 <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
                                     <ShoppingBagIcon className="size-5" />
                                     <span className="badge badge-sm badge-primary indicator-item">
-                                        {products.length}
+                                        {products?.length}
                                     </span>
                                 </div>
                             </div>
